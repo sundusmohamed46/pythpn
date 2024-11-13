f8f883ab3aad99e993001b3f58605e7e2ee6817c
sundus mohamed <sundusmohamed641@gmail.com>
2024-11-13 21:54:28 +0300

    Add files via upload

---
+++ suuuuuuu.py
@@ -0,0 +1,9 @@
+## Samay variable haynayaa magacaaga ka dibna soo daabac data type ka
+
+## Samay list haynaya favourite foods kaaga 
+
+
+# samay list haynaya magacaa maadooyinka aad jeceshahay ka dibna soo daabac
+
+## samay variable haynay age kaaga , sidoo kale samay variable haynaya lambarkaaga , sidoo kale samay variable m
+## magacaaga haynaya ka dib 3 oo wada socda hal mar daabac
